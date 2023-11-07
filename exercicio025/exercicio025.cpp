6661787d658a4d071c449943a9687d9a816041e4
T2 <T2@DESKTOP-KGH9MBO>
2023-11-07 15:48:23 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio025/exercicio025.cpp
@@ -0,0 +1,18 @@
+/*
+exercicio025.cpp
+
+escreva um programa que mostre na tela os numeros de 10 até 0
+*/ 
+
+
+#include <iostream>
+
+int main()
+{
+	for (int i = 10; i >= 0; i--)
+	{
+		printf_s("%d \n", i);
+	}
+}
+
+
